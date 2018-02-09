2cb8f7b33c970778a82acae10470d6bce4b9250d
xxswingxx <vidadelaempresa@gmail.com>
2018-02-09 13:00:43 +0000

    Normalize addresses

--- lib/valbn.rb
+++ lib/valbn.rb
@@ -46,23 +46,60 @@ class Valbn
     case @country
     when *EU_COUNTRIES
       if defined?(Valvat) == 'constant'
-        Valvat.new(@number).exists?(options)
+        result = Valvat.new(@number).exists?(options)
+
+        if options[:formatted] == true && !result.nil? && result.is_a?(Hash)
+          result = {
+            original_format: result,
+            name: result[:name],
+            street_line_1: result[:address]
+          }
+        end
       else
         raise StandardError, 'WARNING: Valvat not found. Please run `gem install valvat`.'
       end
     when 'AU'
       if defined?(Valvat) == 'constant'
-        Valabn.new(@number).exists?(options)
+        result = Valabn.new(@number).exists?(options)
+
+        if options[:formatted] == true && !result.nil? && result.is_a?(Hash)
+          begin
+            business = result[:search_by_ab_nv201408_response][:abr_payload_search_results][:response][:business_entity201408]
+          rescue
+            business = {}
+          end
+
+          result = {
+            original_format: result,
+            name: (business[:main_name] || {})[:organisation_name],
+            region: (business[:main_business_physical_address] || {})[:state_code],
+            postal_code: (business[:main_business_physical_address] || {})[:postcode]
+          }
+        end
       else
         raise StandardError, 'WARNING: Valnzbn not found. Please run `gem install valabn`.'
       end
     when 'NZ'
       if defined?(Valnzbn) == 'constant'
-        Valnzbn.new(@number).exists?(options)
+        result = Valnzbn.new(@number).exists?(options)
+        if options[:formatted] == true && !result.nil? && result.is_a?(Hash)
+          address = result[:registeredAddress] || [{}]
+
+          result = {
+            original_format: result,
+            name: result[:entityName],
+            street_line_1: address.first[:address1],
+            street_line_2: address.first[:address2],
+            city: address.first[:address3],
+            postal_code: address.first[:postCode]
+          }
+        end
       else
         raise StandardError, 'WARNING: Valnzbn not found. Please run `gem install valnzbn`.'
       end
     end
+
+    result
   end
   alias_method :exist?, :exists?
 end
